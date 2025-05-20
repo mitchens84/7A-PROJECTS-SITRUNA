ea5ce7793ad47f47b2cc62dba0fc19b4b968b503
Your Name <your_email@example.com>
2025-05-20 14:40:24 +0700

    Correct base path and homepage for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PAGES/7A-PROJECTS-SITRUNA/', // For GitHub Pages deployment
+  base: '/7A-PROJECTS-SITRUNA/', // For GitHub Pages deployment
 })
